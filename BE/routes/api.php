25db4489098bcf974cfb1bc284ba0397f16b1eda
Makrion <miklaziz3112000@gmail.com>
2022-12-13 17:55:06 +0200

    feat: add-passport-oauth

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/user', function (Request $request) {
+    //return $request->user();
+    return 'hello';
 });
