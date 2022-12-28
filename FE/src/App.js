c87ca513d4081a8e675cad6cf47b30bb077b3517
Tarek Yasser <tarekspeed7@gmail.com>
2022-12-28 03:09:34 +0200

    Add `create_match` to routes

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './NavBar/Navbar'
 import Home from './Home/home'
 import NotFound from './NotFound'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { CreateMatch } from './CreateMatch/CreateMatch'
 
 const App = () => {                           
 
@@ -12,6 +13,10 @@ const App = () => {
         <Navbar></Navbar>
       <div className="content">
           <Switch>
+            <Route exact path="/create_match">
+                <CreateMatch></CreateMatch>
+            </Route>
+
             <Route exact path="/">
                 <Home></Home>
             </Route>
